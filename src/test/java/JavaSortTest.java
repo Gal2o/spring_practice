ea650909347fc794cc62e95cb806d975ca538f9a
gal2o <hunmin8788@gmail.com>
2022-02-08 12:15:04 +0900

    feat: create JavaSort
    자바소트 생성!

---
+++ src/test/java/JavaSortTest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class JavaSortTest {
+
+    private final JavaSort<Integer> javaSort = new JavaSort<>();
+
+    @Test
+    void 정렬하기() {
+        // Given
+        List<Integer> input = List.of(5, 3, 1, 4, 2);
+
+        // When
+        List<Integer> output = javaSort.sort(input);
+
+        // Then
+        assertEquals(List.of(1, 2, 3, 4, 5), output);
+    }
+}
