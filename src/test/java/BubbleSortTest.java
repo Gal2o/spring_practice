ce90a7cf0ce8d24e0f273f23052006b7ab130319
gal2o <hunmin8788@gmail.com>
2022-02-08 11:52:59 +0900

    implement main, bubblesort

---
+++ src/test/java/BubbleSortTest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class BubbleSortTest {
+
+    private final BubbleSort<Integer> bubbleSort = new BubbleSort<>();
+
+    @Test
+    void 정렬하기() {
+        // Given
+        List<Integer> input = List.of(5, 3, 1, 4, 2);
+
+        // When
+        List<Integer> output = bubbleSort.sort(input);
+
+        // Then
+        assertEquals(List.of(1, 2, 3, 4, 5), output);
+    }
+}
